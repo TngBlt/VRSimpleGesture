baf26badcbadde700ee125789fde4bfd28c57654
unknown <j.hoengaard@gmail.com>
2018-10-03 16:09:43 +0200

    Simple Gesture prefabs

---
+++ SimpleGestureVR/VRGesture/Recenter.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.VR;
+
+namespace FrameSynthesis.VR
+{
+    public class Recenter : MonoBehaviour
+    {
+        void Start()
+        {
+            UnityEngine.XR.XRDevice.SetTrackingSpaceType(UnityEngine.XR.TrackingSpaceType.Stationary);
+        }
+
+        void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.R))
+            {
+                UnityEngine.XR.InputTracking.Recenter();
+            }
+        }
+    }
+}
